38f5d54b34d88f55d1c107f59ceb566e373c675e
SilverPsalm <171736682+silverpsalm@users.noreply.github.com>
2025-06-10 22:16:45 -0400

    Add more parameters to functions. Adjust comments.

--- library.js
+++ library.js
@@ -1,15 +1,19 @@
 const myLibrary = [];
 
-function Book(title,author,pages) {
+function Book(title,author,pages,status,summary) {
 
 }
 
-function addBookToLibrary(title,author,pages) {
+function addBookToLibrary(title,author,pages,status,summary) {
 // 1. create new Book object
-    let title = new Book();
-// generate unique id:
+    let title = new Book(title,author,pages,status,summary);
+// generate unique id. Must be defined here to avoid inheriting prototype's uuid...?
     title.id = crypto.randomUUID();
 // store new obj in myLibrary
     myLibrary.push(title);
 // 
+}
+
+function addLibraryToPage() {
+    // iterate over myLibrary array to add books to html page.
 }
