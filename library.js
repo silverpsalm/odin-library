be72f3630682524d016e9d2e1b880aaeb86c4db0
SilverPsalm <171736682+silverpsalm@users.noreply.github.com>
2025-06-13 21:27:49 -0400

    Store card template html in javascript variables. Assigned card template parts to variables for future customization with function parameters.

--- library.js
+++ library.js
@@ -16,4 +16,28 @@ function addBookToLibrary(title,author,pages,status,summary) {
 
 function addLibraryToPage() {
     // iterate over myLibrary array to add books to html page.
+
+    // the for...each card template:
+
+    // create the card elements:
+    const bookCard = document.createElement("div");
+    const bookTitle = document.createElement("p");
+    const bookBy = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const bookStatus = document.createElement("p");
+
+    // assign classes to the created elements:
+    bookCard.classList.add("book");
+    bookTitle.classList.add('title');
+    bookBy.classList.add('author');
+    bookPages.classList.add('pages');
+    bookStatus.classList.add('status');
+
+    // append them to the first:
+    let cardParts = [bookTitle,bookBy,bookPages,bookStatus];
+
+    bookCard.appendChild(cardParts);
+
+    // NOW... we customize card parts with the function parameters.
+
 }
